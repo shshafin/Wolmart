f724c8476f632039e74f31dd04881657101a0cde
shshafin <shafinsadnan08@gmail.com>
2024-03-30 05:15:57 +0600

    all

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    'use strict'
+    // Slick Slider Start
+    
+$('#banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    dots:true,
+  });
+
+  // Blog calender
+
+});
